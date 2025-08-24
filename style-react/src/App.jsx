330d03f6b6b3fd6ac4ea93e47832a64bc321663a
shaay98 <nrichardson567@icloud.com>
2025-08-24 08:53:56 +0000

    scss app

---
+++ style-react/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.scss'
+
+function App() {
+  return (
+    <>
+      <div>
+      <div className="section-one">
+        <h2>Section One</h2>
+        <a href="#">Learn more</a>
+      </div>
+
+      <div className="section-two">
+        <button>Click Me</button>
+      </div>
+
+      <div className="section-three">
+        <p>This is section three content.</p>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
